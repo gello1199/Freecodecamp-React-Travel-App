db0261b705a549311fb34cf6dfbcbe48cf067087
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-13 16:23:39 -0500

    Add data to data.js file

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+    },
+    {
+        title: "The Collosseum",
+        location: "Italy",
+        googleMapsUrl: "https://www.google.com/maps/place/Colosseum/@41.8902102,12.4922309,15z/data=!4m5!3m4!1s0x0:0x28f1c82e908503c4!8m2!3d41.8902102!4d12.4922309",
+        startDate: "15 March 2022",
+        endDate: "30 March 2022",
+        description: "The Collosseum is a monumental 3-tiered Roman amphitheater once used for gladiatorial games, with guided tour option.",        
+        imageUrl: "https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1396&q=80"
+    },
+    {
+        title: "The Acroplois of Athens",
+        location: "Greece",
+        googleMapsUrl: "https://www.google.com/maps/place/Acropolis+of+Athens/@37.9715323,23.7235605,17z/data=!3m1!4b1!4m5!3m4!1s0x14a1bd1837f5acf3:0x5c97c042f5eb0df6!8m2!3d37.9715323!4d23.7257492",
+        startDate: "15 September 2022",
+        endDate: "30 September 2022",
+        description: "The Acropolis are ruins of iconic 5th-century B.C. temple complex on Athens' rocky hilltop undergoing restoration.",
+        imageUrl: "https://images.unsplash.com/photo-1584721284569-c5f83d57ec78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2833&q=80"
+    }
+
+]
